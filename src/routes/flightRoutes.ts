131873e8012c9ed76c0a0f8eae0f3123bbb2923f
josejuniorpg <yunipgx@protonmail.com>
2024-09-07 18:28:55 -0600

    Fix bug with the dependecies.

--- src/routes/flightRoutes.ts
+++ src/routes/flightRoutes.ts
@@ -1,8 +1,13 @@
 import { Router } from 'express';
 import { FlightController } from '../controllers/FlightController';
+import {FlightRepository} from "../repositories/FlightRepository";
+import {FlightService} from "../services/FlightService";
 
 const router = Router();
-const flightController = new FlightController();
+
+const flightRepository = new FlightRepository();
+const flightService = new FlightService(flightRepository);
+const flightController = new FlightController(flightService);
 
 router.get('/', flightController.getAllFlights.bind(flightController));
 router.post('/create', flightController.createFlight.bind(flightController));
